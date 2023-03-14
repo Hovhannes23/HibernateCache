d5a1cf8e5a81d788e7620b47d4eed82cf4ec32aa
Ovannes Shaumyan <ovannes_shaumyan@senla.eu>
2023-03-14 20:29:54 +0300

    add query cache example

--- src/main/java/com/example/hibernatecache/HibernateCacheApplication.java
+++ src/main/java/com/example/hibernatecache/HibernateCacheApplication.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.internal.SessionFactoryImpl;
+import org.hibernate.query.Query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,17 +22,21 @@ public class HibernateCacheApplication {
         berlin.setName("Berlin");
         berlin.setPopulation(3_500_000L);
         City praga = new City();
-        berlin.setName("Praga");
-        berlin.setPopulation(1_35_000L);
+        praga.setName("Praga");
+        praga.setPopulation(1_350_000L);
         City london = new City();
-        berlin.setName("London");
-        berlin.setPopulation(5_800_000L);
+        london.setName("London");
+        london.setPopulation(5_800_000L);
 
         // save city to db
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
         session.save("berlin", berlin);
+        session.save("praga", praga);
+        session.save("london", london);
         session.close();
+
+
 
         // get city in session1
         Session session1 = factory.openSession();
@@ -51,6 +56,38 @@ public class HibernateCacheApplication {
         City city2 = session2.get(City.class, 1);
         System.out.println(city2);
         session2.close();
+
+
+
+        // query cache
+        Session session3 = factory.openSession();
+        Query query = session3.createQuery("FROM City c WHERE c.population > :population");
+        query.setParameter("population", new Long(1_400_000));
+        query.setCacheable(true);
+        System.out.println(query.list());
+
+        // в той же сессии создаем запрос с тем же синтаксисом и значением параметра.
+        // Ожидаемый результат: данные из кэша
+        Query query2 = session3.createQuery("FROM City c WHERE c.population > :population");
+        query2.setParameter("population", new Long(1_400_000));
+        query2.setCacheable(true);
+        System.out.println(query2.list());
+
+        // в той же сессии создаем запрос с тем же синтаксисом и меняем значение параметра.
+        // Ожидаемый результат: данные из БД
+        Query query3 = session3.createQuery("FROM City c WHERE c.population > :population");
+        query3.setParameter("population", new Long(1_444_444));
+        query3.setCacheable(true);
+        System.out.println(query3.list());
+
+        // в другой сессии создаем запрос с тем же синтаксисом и значением параметра.
+        // Ожидаемый результат: данные из кэша
+        Session session4 = factory.openSession();
+        Query query1 = session4.createQuery("FROM City c WHERE c.population > :population");
+        query1.setParameter("population", new Long(1_400_000));
+        query1.setCacheable(true);
+        System.out.println(query1.list());
+
     }
 
 }
